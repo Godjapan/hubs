aab62383d9a81c0f99e55ef0ee3a6021ef4e4729
Godjapan <info@godjapan.tokyo>
2022-10-09 22:36:10 +0900

    classname etc

--- src/react-components/room/LoadingScreen.js
+++ src/react-components/room/LoadingScreen.js
@@ -3,25 +3,25 @@ import PropTypes from "prop-types";
 import { LoadingScreenLayout } from "../layout/LoadingScreenLayout";
 import { Spinner } from "../misc/Spinner";
 import { useRandomMessageTransition } from "./useRandomMessageTransition";
-import cyzyHowToControlImg from "../../assets/images/cyzy-how-to-control.png";
+import MetawithHowToControlImg from "../../assets/images/metawith_howto_control.png";
 export function LoadingScreen({ message, infoMessages }) {
   const infoMessage = useRandomMessageTransition(infoMessages);
   return (
     <LoadingScreenLayout
       center={
         <>
-          <div className="cyzy-loading-message-wrap">
-            <div className="cyzy-spinner-wrap">
+          <div className="metawith-loading-message-wrap">
+            <div className="metawith-spinner-wrap">
               <Spinner />
             </div>
             <p>{message}</p>
           </div>
-          <img src={cyzyHowToControlImg} className="cyzy-how-to-control" alt={"how to control"} />
+          <img src={MetawithHowToControlImg} className="metawith-howto-control" alt={"how to control"} />
         </>
       }
       bottom={
         <>
-          <div className="cyzy-loading-tips-wrap">
+          <div className="metawith-loading-tips-wrap">
             <h3>{infoMessage.heading}</h3>
             <p>{infoMessage.message}</p>
           </div>
